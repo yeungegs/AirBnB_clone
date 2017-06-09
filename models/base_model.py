eb6b9e0ae11e49f8ced2998300072600ba1b7636
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-09 01:28:00 -0700

    Update work in progress base_model

--- models/base_model.py
+++ models/base_model.py
@@ -2,23 +2,39 @@
 
 from datetime import datetime, date, time
 import uuid
+"""
+This is the BaseModel module
+It contains one class: BaseModel
+It contains the following functions:
+    def to_json(self)
+"""
 
 class BaseModel:
     """docstring
     """
     def __init__(self):
-        """public instance methods"""
+        """public instance attributes
+        id: string - assign with an uuid when an instance is created
+        created_at: datetime - assign with the current
+        datetime when an instance is created
+        updated_at: datetime
+        """
         self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
     def __str__(self):
-        """docstring """
+        """private method
+        __str__: should print: [<class name>] (<self.id>) <self.__dict__>
+         """
         return("[{}] {{}) {}".format(self.__class__.name__,
                                       self.id,
                                       self.__dict__))
     def __save__(self):
-        """docstring"""
+        """public instance method"""
         self.updated_at = datetime.now()
-        
-        
-    
+
+    def to_json(self):
+        """public instance method"""
+        json = self.__dict__
+        json['class'] = self.__class__.__name__
+        return(json)
