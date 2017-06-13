6aef1ab76a2d351dffe914c25b63b88a9a88e728
spacexengineer <hunter.casbeer@gmail.com>
2017-06-13 09:19:02 -0700

    implement list of class to reload new classes

--- scripts/models/engine/file_storage.py
+++ scripts/models/engine/file_storage.py
@@ -2,12 +2,15 @@
 """Saving an object to a file
 """
 import json
-import models
 from models.base_model import BaseModel
-from datetime import datetime
-
-
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 # time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class FileStorage:
     """serializes instances to a JSON file
@@ -16,50 +19,90 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "Amenity": Amenity,
+        "City": City,
+        "Place": Place,
+        "Review": Review,
+        "State": State
+    }
+
     def all(self):
         """Returns objects
         """
-        print("\nall IS USED\n")
+        # print("\nFILE all IS USED\n")
         return (FileStorage.__objects)
 
     def new(self, obj):
         """Sets in __obj the obj with
         key <obj class name>.id
         """
-        print("\nnew HAS STARTED\n")
+        # print("\nFILE new HAS STARTED\n")
         new_obj_id = "{}.{}".format(type(obj).__name__,
-                                 obj.id)
+                                    obj.id)
+        # print("This is the new_obj {}".format(new_obj_id))
         FileStorage.__objects[new_obj_id] = obj
-        print("\nnew IS COMPLETED\n")
+        # print("This is __objects dictionary {}".format(self.__objects))
+        # print("\nFILE new IS COMPLETED\n")
 
     def save(self):
         """serializes __objects to the JSON file
         """
-        print("\nsave HAS STARTED\n")
+        # print("\nFILE save HAS STARTED\n")
         new_dict = {}
         for key in FileStorage.__objects.keys():
             new_dict[key] = FileStorage.__objects[key].to_json()
-        with open(FileStorage.__file_path, mode="w", encoding="UTF-8") as to_file:
+            # print("This is new_dict --> {}".format(new_dict))
+        with open(FileStorage.__file_path, mode="w",
+                  encoding="UTF-8") as to_file:
+            # serialize here
             (json.dump(new_dict, to_file))
-        print("\nsave IS COMPLETED\n")
+        # print("\nFILE save IS COMPLETED\n")
 
     def reload(self):
         """deserializes the JSON file to __objects
         (only if the JSON file exists ; otherwise, do nothing)
         """
-        print("\nreload HAS STARTED\n")
         try:
-            print("\nreload SUCCESSFUL TRY\n")
-            with open(FileStorage.__file_path, mode="r", encoding="UTF-8") as to_file:
-                object_loaded = json.load(to_file)
-            for key, value in object_loaded.items():
-                print("\nreload LOOP ENTERED\n")
-                class_function = value["__class__"]
-                FileStorage.__objects[key] = class_function(**object_loaded[item])
-                
+            # print("\nATTEMPTING TRY reload\n")
+            with open(FileStorage.__file_path, mode="r",
+                      encoding="UTF-8") as to_file:
+                # print("\nRELOAD File opened\n")
+                objects_loaded = json.load(to_file)
+                from models.base_model import BaseModel
+                from models.amenity import Amenity
+                from models.city import City
+                from models.place import Place
+                from models.review import Review
+                from models.state import State
+                from models.user import User
+
+                class_list = ["BaseModel", "Amenity", "City", "Place",
+                              "Review", "State", "User"]
+
+                for key, value in objects_loaded.items():
+                    if value.get("__class__") in class_list:
+                        meth = value.get("__class__")
+                        self.__objects[key] = eval(
+                            str(meth))(objects_loaded[key])
+
+                """
+                for key in json_obj.keys():
+                    self.__objects[key] = BaseModel(**json_obj[key])
+
+                for obj_id, dic in object_loaded.items():
+                    # print("\nreload LOOP ENTERED\n")
+                    obj_class = objects_loaded[obj_id].get("__class__")
+                    if obj_class in classes:
+                        meth = objects_loaded.get(obj_class)
+                        FileStorage.__objects[obj_id] = meth(
+                            **objects_loaded[obj])
+                """
+            # print("\nSUCCESSFUL TRY\n")
         except:
-            print("\nreload TRIED FAILED\n")
+            # print("\nreload EXCEPT USED\n")
             pass
-        print("\nreload IS COMPLETED\n")
 
-
+        # print("\nreload IS COMPLETED\n")
