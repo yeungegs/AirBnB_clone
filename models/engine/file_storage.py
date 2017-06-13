a9c2b887c473f381e8461ab1a55890a927d4e9f2
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-13 08:20:03 -0700

    More fixes to file_storage - recovered file from vagrant box destroy

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,6 +31,12 @@ class FileStorage:
         """Public instance method:
         serializes __objects to the JSON file (path: __file_path)
         """
+        human_read = {}
+        for key, value in self.__objects.items():
+            human_read[k] = v.to_json()
+        with open(self.__file_path, mode="w", encoding="UTF-8") as fhandle:
+            json.dump(printable, fhandle)
+        
 
     def reload(self):
         """Public instance method:
@@ -38,3 +44,16 @@ class FileStorage:
         
         * should only do this if the JSON file exists ; otherwise, do nothing)
         """
+        from models.base_model import BaseModel
+
+        try:
+            with open(self.__file_path, mode="r", encoding="UTF-8") as fhandle:
+                human_read = json.load(fhandle)
+            for i, j in human_read.items():
+                self.__objects[i] = BaseModel(*j)
+        except FileNotFoundError:
+            pass
+
+        def userSerialize(obj):
+            if isinstance(obj, datetime):
+                return obj
