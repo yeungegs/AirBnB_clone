28b153846dc4b3a68c78a29157a5eaae02993da2
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-13 07:17:42 -0700

    Add WIP file_storage :construction:

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,40 @@
-Private class attributes:
-__file_path: string - path to the JSON file (ex: file.json)
-__objects: dictionary - empty but will store all objects by <class name>.id (ex: to store a BaseModel object with id=12121212, the key will be BaseModel.12121212)
-Public instance methods:
-all(self): returns __objects
-new(self, obj): sets in __objects the obj with key <obj class name>.id
-save(self): serializes __objects to the JSON file (path: __file_path)
-reload(self): deserializes the JSON file to __objects (only if the JSON file exists ; otherwise, do nothing)
+#!/usr/bin/python3
+import json
+import datetime
+"""
+This is module file_storage
+This module contains one class: FileStorage
+"""
+
+
+
+class FileStorage:
+    """
+    Serialize and deserialize objects in models to json
+    Private class attributes:
+    __file_path: string - path to the JSON file (ex: file.json)
+    __file_path = "file.json"
+    __objects = {}
+    """
+    def all(self):
+        """Public instance method: Returns __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """Public instance method:
+        sets in __objects the obj with key <obj class name>.id
+        """
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """Public instance method:
+        serializes __objects to the JSON file (path: __file_path)
+        """
+
+    def reload(self):
+        """Public instance method:
+        deserializes the JSON file to __objects
+        
+        * should only do this if the JSON file exists ; otherwise, do nothing)
+        """
