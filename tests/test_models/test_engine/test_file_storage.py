d971eeee53f3f159d885eb7500273433b408eec2
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-13 15:17:23 -0700

    Update file_storage tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,48 @@
+import unittest
+import os.path
+from datetime import datetime
+from models.engine.file_storage import FileStorage
+
+class Test_FileStorage(unittest.TestCase):
+    """
+    Test the file storage class
+    """
+
+    def setUp(self):
+        self.store = FileStorage()
+        self.model = BaseModel(test_args)
+        self.test_len = 0
+        if os.path.isfile("file.json"):
+            self.test_len = len(self.store.all())
+
+    def tearDown(self):
+        import os
+        if os.path.isfile("file.json"):
+            os.remove('file.json')
+
+    def test_all(self):
+        self.assertEqual(len(self.store.all()), self.test_len)
+
+    def test_new(self):
+        self.assertEqual(len(self.store.all()), self.test_len)
+        self.model.save()
+        self.assertEqual(len(self.store.all()), self.test_len + 1)
+        a = BaseModel()
+        a.save()
+        self.assertEqual(len(self.store.all()), self.test_len + 2)
+
+    def test_save(self):
+        self.test_len = len(self.store.all())
+        a = BaseModel()
+        a.save()
+        self.assertEqual(len(self.store.all()), self.test_len + 1)
+        b = User()
+        self.assertNotEqual(len(self.store.all()), self.test_len + 2)
+        b.save()
+        self.assertEqual(len(self.store.all()), self.test_len + 2)
+
+    def test_reload(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
